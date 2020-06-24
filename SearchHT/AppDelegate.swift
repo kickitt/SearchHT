1aa6f877c8e75a2f2833b742839fac8f162f94ff
Roman Berezin <romanberezinua@gmail.com>
2020-06-24 16:58:42 +0300

    Initial commit

---
+++ SearchHT/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SearchHT
+//
+//  Created by Roman Berezin on 24.06.2020.
+//  Copyright © 2020 Roman Berezin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
